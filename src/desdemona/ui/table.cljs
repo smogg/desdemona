022a3f7e6d2f6d1c5c92038c9da05dc300777f16
Oskar Zabik <oskar.zabik@gmail.com>
2016-04-26 15:28:24 +0200

    fixing state references & ordering

--- src/desdemona/ui/table.cljs
+++ src/desdemona/ui/table.cljs
@@ -2,13 +2,11 @@
   (:require [wilson.dom :as wd]
             [reagent.session :as session]))
 
-(def rows (:results @session/state))
-
-(def all-keys (-> (mapcat wd/get-all-keys rows) distinct wd/prepare-keys))
-
 (defn table-component
   []
-  (let [sorted-rows (wd/sort-rows rows
+  (let [rows (:results @session/state)
+        all-keys (-> (mapcat wd/get-all-keys rows) distinct wd/prepare-keys)
+        sorted-rows (wd/sort-rows rows
                                   {:default
                                    (fn [k rows]
                                      (if (= (:table-sort-order @session/state)
@@ -17,15 +15,17 @@
                                        (reverse (sort-by k rows))))}
                                   (:table-sort-key @session/state))
         get-new-order (fn [state k]
-                        (let [sort-key (:table-sort-key @session/state)
-                              sort-order (:table-sort-order @session/state)
-                              swap-order {:asc :desc :desc :asc}]
+                        (let [sort-key (:table-sort-key @state)
+                              sort-order (:table-sort-order @state)
+                              swap-order {:asc :desc :desc :asc}
+                              sort-key-desc (wd/describe-key sort-key)
+                              k-desc (wd/describe-key k)]
                           {:table-sort-key k
-                           :table-sort-order (if (= sort-key k)
+                           :table-sort-order (if (= sort-key-desc k-desc)
                                                (swap-order sort-order)
                                                :asc)}))
         update-state-order
-        #(session/swap! merge (get-new-order @session/state %))]
+         #(session/swap! merge (get-new-order session/state %))]
     [:div {:class "container-fluid"}
      [:div {:class "table-responsive"}
       (wd/with-class "table-condensed"
@@ -34,6 +34,6 @@
          sorted-rows
          {:k->attrs (fn [k]
                       {:on-click #(update-state-order k)
-                       :class (when (= k (:table-sort-key @session/state))
+                       :class (when (= k (:table-sort-key session/state))
                                 (name
                                  (:table-sort-order @session/state)))})}))]]))
