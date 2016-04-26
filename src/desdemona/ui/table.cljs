7933e1bbfd4cc2bd8d53950d531363807fcd383e
Oskar Zabik <oskar.zabik@gmail.com>
2016-04-26 16:13:51 +0200

    use passed state and make key comparison work

--- src/desdemona/ui/table.cljs
+++ src/desdemona/ui/table.cljs
@@ -3,17 +3,17 @@
             [reagent.session :as session]))
 
 (defn table-component
-  []
-  (let [rows (:results @session/state)
+  [state]
+  (let [rows (:results @state)
         all-keys (-> (mapcat wd/get-all-keys rows) distinct wd/prepare-keys)
         sorted-rows (wd/sort-rows rows
                                   {:default
                                    (fn [k rows]
-                                     (if (= (:table-sort-order @session/state)
+                                     (if (= (:table-sort-order @state)
                                             :asc)
                                        (sort-by k rows)
                                        (reverse (sort-by k rows))))}
-                                  (:table-sort-key @session/state))
+                                  (:table-sort-key @state))
         get-new-order (fn [state k]
                         (let [sort-key (:table-sort-key @state)
                               sort-order (:table-sort-order @state)
@@ -25,7 +25,7 @@
                                                (swap-order sort-order)
                                                :asc)}))
         update-state-order
-         #(session/swap! merge (get-new-order session/state %))]
+         #(session/swap! merge (get-new-order state %))]
     [:div {:class "container-fluid"}
      [:div {:class "table-responsive"}
       (wd/with-class "table-condensed"
@@ -34,6 +34,6 @@
          sorted-rows
          {:k->attrs (fn [k]
                       {:on-click #(update-state-order k)
-                       :class (when (= k (:table-sort-key session/state))
+                       :class (when (= (wd/describe-key k) (wd/describe-key (:table-sort-key @state)))
                                 (name
-                                 (:table-sort-order @session/state)))})}))]]))
+                                 (:table-sort-order @state)))})}))]]))
